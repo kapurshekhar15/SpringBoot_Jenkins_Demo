fd9f1827211c19588573502945a5d66310ff495c
kapurs15 <kapurshekhar15@gmail.com>
2025-04-23 16:07:16 +0530

    file added

---
+++ src/test/java/com/example/jenkins/jenkinsdemo/JenkinsdemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.jenkins.jenkinsdemo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class JenkinsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
